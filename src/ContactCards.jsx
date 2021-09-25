9de9c7f3ec884a422969f473980dcda702df1d9e
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-25 12:07:36 +0300

    display dynamic data and add uuid for key

--- src/ContactCards.jsx
+++ src/ContactCards.jsx
@@ -1,229 +1,43 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ContactCards = ({ contactList }) => {
     contactList && console.log(contactList);
 
     return (
         <>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
-            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
-                <img
-                    src='https://randomuser.me/api/portraits/men/7.jpg'
-                    alt='user'
-                    className='w-32 rounded-full mx-auto'
-                />
-                <figcaption className='text-center mt-5 text-gray-500'>
-                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
-                        Max
-                    </p>
-                    <p>
-                        <span className='font-medium'>email:</span>{' '}
-                        hahahah@gmail.com
-                    </p>
-                    <p>
-                        <span className='font-medium'>phone:</span> 0928292828
-                    </p>
-                    <p>
-                        <span className='font-medium'>city:</span> London
-                    </p>
-                </figcaption>
-            </figure>
+            {contactList?.map((contact) => (
+                <figure
+                    className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'
+                    key={uuidv4()}
+                >
+                    <img
+                        src={contact.picture?.medium}
+                        alt='user'
+                        className='w-28 rounded-full mx-auto'
+                    />
+                    <figcaption className='text-center mt-5 text-gray-500'>
+                        <p className='text-2xl text-gray-700 font-semibold mb-2'>
+                            {contact.name?.first} {contact.name?.last}
+                        </p>
+                        <p>
+                            <span className='font-medium'>email:</span>{' '}
+                            {contact.email}
+                        </p>
+                        <p>
+                            <span className='font-medium'>phone:</span>{' '}
+                            {contact.phone}
+                        </p>
+                        <p>
+                            <span className='font-medium'>city:</span>{' '}
+                            {contact.location?.city}
+                        </p>
+                        <p>
+                            <span className='font-medium'>country:</span>{' '}
+                            {contact.location?.country}
+                        </p>
+                    </figcaption>
+                </figure>
+            ))}
         </>
     );
 };
