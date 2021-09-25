c4c191b106dde12eff16bb6e7450fa20a58f9d70
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-25 11:33:40 +0300

    style card

--- src/ContactCards.jsx
+++ src/ContactCards.jsx
@@ -1,13 +1,27 @@
 const ContactCards = () => {
     return (
         <>
-            <figure className='bg-gray-500 h-80 rounded-lg shadow-md pt-7'>
+            <figure className='bg-gray-200 h-80 rounded-lg shadow-md pt-7'>
                 <img
                     src='https://randomuser.me/api/portraits/men/7.jpg'
                     alt='user'
                     className='w-32 rounded-full mx-auto'
                 />
-                <figcaption></figcaption>
+                <figcaption className='text-center mt-5 text-gray-500'>
+                    <p className='text-2xl text-gray-700 font-semibold mb-2'>
+                        Max
+                    </p>
+                    <p>
+                        <span className='font-medium'>email:</span>{' '}
+                        hahahah@gmail.com
+                    </p>
+                    <p>
+                        <span className='font-medium'>phone:</span> 0928292828
+                    </p>
+                    <p>
+                        <span className='font-medium'>city:</span> London
+                    </p>
+                </figcaption>
             </figure>
         </>
     );
